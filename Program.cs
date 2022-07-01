fea5723fc5b172c20840e3488330e118482f4c50
BuildTools <richard.mazanek@gmail.com>
2022-06-30 23:55:14 -0400

    didn't realize download process needed an exe: ffmpeg.exe. you'll need to put this in the root folder to get this to work

--- Program.cs
+++ Program.cs
@@ -11,10 +11,13 @@ namespace TwitchTranscriber
   {
     class Video
     {
+      public string? broadcaster_id {get; set;}
+      public string? broadcaster_name {get; set;}
       public string created_at {get; set;}
       public string description {get; set;}
       public string duration {get; set;}
       public string id {get; set;}
+      public string? video_id {get; set;}
       public string language {get; set;}
       public string? muted_segments {get; set;}
       public string published_at {get; set;}
@@ -40,33 +43,54 @@ namespace TwitchTranscriber
     }
     static async Task Main(string[] args)
     {
-      string jsonFileName = "Clips copy.json";
+      string jsonFileName = "ArchivedVideos.json";
       string jsonString = File.ReadAllText(jsonFileName);
       Videos videos = JsonSerializer.Deserialize<Videos>(jsonString);
       
-      List<VideoDownloadOptions> downloadOptions = CreateDownloadOptions(videos);
+      List<VideoDownloadOptions> downloadOptions = CreateDownloadOptions(videos, true);
       
       await DownloadAllVideosAsync(downloadOptions);
     }
-    static List<VideoDownloadOptions> CreateDownloadOptions(Videos videos)
+    static List<VideoDownloadOptions> CreateDownloadOptions(Videos videos, bool test=false)
     {
       string quality = "160p";
       bool cropBeginning = false;
       bool cropEnding = false;
       int downloadThreads = 10;
-      string tempFolder = "D:\\Repos\\Projects\\Twitch Data\\Files";
+      string tempFolder = "D:\\Repos\\Projects\\TwitchTranscriber\\Files\\";
 
       List<VideoDownloadOptions> downloadOptions = new List<VideoDownloadOptions>();
       
-      for (int i = 0; i < videos.data.Length; i++)
+      int numberOfVidoesToProcess;
+      if(test)
+      {
+        numberOfVidoesToProcess = 3;
+      }
+      else
+      {
+        numberOfVidoesToProcess = videos.data.Length;
+      }
+
+      for (int i = 0; i < numberOfVidoesToProcess; i++)
       {
         VideoDownloadOptions videoDownloadOptions = new VideoDownloadOptions();
         videoDownloadOptions.Id = Int32.Parse(videos.data[i].id);
         videoDownloadOptions.Quality = quality;
         videoDownloadOptions.CropBeginning = cropBeginning;
-        videoDownloadOptions.CropEnding = cropEnding;
+
+        if(test)
+        {
+          videoDownloadOptions.CropEnding = true;
+          videoDownloadOptions.CropEndingTime = 5.0;
+        }
+        else
+        {
+          videoDownloadOptions.CropEnding = cropEnding;
+        }
+
         videoDownloadOptions.DownloadThreads = downloadThreads;
         videoDownloadOptions.TempFolder = tempFolder;
+        videoDownloadOptions.FfmpegPath = "ffmpeg.exe"; //using Xabe.FFmpeg.Downloader;
         videoDownloadOptions.Filename = $"{videos.data[i].id}_{videos.data[i].created_at}_{videos.data[i].title}";
 
         downloadOptions.Add(videoDownloadOptions);
